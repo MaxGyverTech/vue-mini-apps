509d25698c3f5d31e991eb1abbc83f22a444d949
MaxGyverTech <maksimfadeenko2@gmail.com>
2024-03-31 03:01:05 +0700

    weather fix

--- src/components/WeatherApp.vue
+++ src/components/WeatherApp.vue
@@ -1,16 +1,12 @@
 <script setup lang="ts">
-import { ref, onMounted, computed, watch } from 'vue';
+import { ref, onMounted,  watch } from 'vue';
 import { type Forecast } from './WeatherItem.vue';
 import WeatherItem from './WeatherItem.vue';
-interface City {
-  lat: number
-  lon: number
-  name: string
-  country: string
-  timezone: string
-}
+
+
 const citySearch = ref('1')
 const city = ref('')
+const isLoading = ref(true)
 const forecasts = ref([] as Forecast[])
 
 let currentLocation: {lat: number, lon: number} = null!
@@ -19,12 +15,14 @@ navigator.geolocation.getCurrentPosition((position) => {
   citySearch.value = ''
 }, (e)=>console.error(e), {maximumAge: 1000})
 watch(citySearch, async () => {
+  isLoading.value = true
   let f: {city: string, forecasts: Forecast[]} = null!
   if (citySearch.value == '' && currentLocation != null) 
       f = await getForecast(currentLocation)
   else f = await getForecast(citySearch.value)
   city.value = f.city
   forecasts.value = f.forecasts
+  isLoading.value = false
 })
 
 async function getForecast(location: string | { lat: number, lon: number} ) : Promise<{ city: string, forecasts: Forecast[] }> {
@@ -60,20 +58,54 @@ onMounted(()=>citySearch.value = '')
 <div id="wrapper">
     <h1>Weather App</h1>
     <input class="centered" type="text" v-model="citySearch" placeholder="Enter city">
-    <div class="centered">{{ city }}</div>
-
+    
+    <div class="centered fade-in" :key="city">{{ city }}</div>
     <div id="forecasts">
-      <WeatherItem v-for="forecast in forecasts" :key="forecast.date" :forecast="forecast"/>
+      <p v-show="isLoading" class="loader "></p>
+      <WeatherItem v-for="(forecast, index) in forecasts" class="fade-in" :style="{ animationDelay: index * 0.05 + 's' }" :key="city+forecast.date" :forecast="forecast"/>
     </div>
 </div>
 </template>
 
 <style scoped>
+.loader {
+  transition: 0.1s;
+  position: absolute;
+  left: calc(50% - 30px);
+  top: calc(50% - 60px);
+  border: 10px solid #5e5e5e; 
+  border-top: 10px solid #d8d8d8; 
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  animation: spin 2s linear infinite;
+}
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+.fade-in {
+  animation: fade-in 0.5s forwards;
+}
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+    transform: translateY(50px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
 .centered {
   display: block;
   margin: 0 auto;
 }
 #forecasts {
+  min-height: 100px;
+  position: relative;
   display: flex;
   margin: 0 auto;
 }
@@ -85,6 +117,7 @@ onMounted(()=>citySearch.value = '')
   font-size: 13px;
 }
 #wrapper {
+  position: relative;
   max-width: 1000px;
   margin: 0 auto;
   padding: 40px;
